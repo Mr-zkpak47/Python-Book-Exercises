8410c19f4f413d36d99830a2c7bc669c6f888818
Mr-zkpak47 <zkpak47@gmail.com>
2023-10-18 16:38:30 +0500

    first commmit

---
+++ animals.py
@@ -0,0 +1,8 @@
+animals:list[str] = ["Dog","Cat","Deer"]
+
+for animal in animals:
+    print(animal)
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("These animals have four legs.")
---
+++ pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ["Fajita", "Pepporina", "Chicken Tikka"]
+
+for pizza in pizzas:
+    print(pizza)
+print("\n")
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("\nI really love pizzas.")
